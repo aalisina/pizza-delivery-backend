6c1861c3aeb256f98ff2edf0f897ecf7b1824e2f
sinazay123 <sinazay123@gmail.com>
2021-08-13 11:39:42 -0500

    Agregar conexion de DB con mongoose

---
+++ dbConnect.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose');
+
+const MONGO_URI ='mongodb://localhost:27017';
+
+mongoose.connect(MONGO_URI, {useUnifiedTopology: true, useNewUrlParser: true});
+
+const db = mongoose.connection;
+
+db.on('connected', ()=> {
+    console.log('DB is connected');
+});
+
+db.on('error', ()=> {
+    console.log('DB connection failed');
+})
+
+module.exports = mongoose;
