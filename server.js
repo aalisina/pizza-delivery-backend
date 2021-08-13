5e8d65d972d444c6b66dca01349dfbf5fc8cdcdb
sinazay123 <sinazay123@gmail.com>
2021-08-13 16:41:02 -0500

    agregar modelo de pizzas y probar que esta funcionando

--- server.js
+++ server.js
@@ -1,12 +1,24 @@
 const express = require('express');
 const db = require('./dbConnect');
 
+const Pizza = require('./models/pizzaModel');
 const app = express();
 
 app.use(express.json());
 
 app.get('/', (req, res)=> {
     res.send('Server is working.');
+});
+
+app.get('/pizzas', async (req, res)=> {
+    const pizzas = await Pizza.find({}, (err, docs)=> {
+        if (err) {
+            console.log(err);
+        } else {
+            res.send(docs)
+        }
+    });
+    
 });
 
 const PORT = process.env.PORT || 5000
